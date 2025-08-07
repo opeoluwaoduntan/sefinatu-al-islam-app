73052e79537a94b97c25e47018b7e3e9fe115690
Oduntan Opeoluwa <opeoluwa509381@gmail.com>
2025-08-07 14:13:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,53 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>SEFINATU AL-ISLAM</h1>
+        <h3>INSTITUTE OF POLICE DISCIPLINE AND DISCIPLINARY STUDIES</h3>
+    <p>In collaboration with <strong>Sefinatu Al-Islam</strong>, affiliate with us and benefits</p>
+    <ul>
+        <li>Course Certificate</li>
+        <li>Earn #3m for promoting Discipline/offcycle retirement.</li>
+        <li>Interest free Loans and credit facilities.</li>
+        <li>Appeal writting for Dismissal or Rank Reduction.</li>
+        <li>Advised on Orderly Room Trial Problems.</li>
+        <li>Hlep SPO with Query and Disciplinary Issues.</li>
+        <li>Aid Dismissed Police Officer get Pension Benefits.</li>
+        <li>Paid #500,000 for Dismissed Officer. </li>
+        <li>#100,000 each for Medical or Legal Assitance.</li>
+        <li>Retirement advice and made easy packages.</li>
+        <li>Debt buying and Finance.</li>
+        <li>Acquire Knowledge and skills on Disciplinary issues.</li>
+        <li>Free Legal Consultations. </li>
+    </ul>
+    <h5 className="protect">PROTECT YOUR JOB AND NEVER WAIT UNTIL TOO LATE!</h5>
+    <p className="phone"><strong>CALL</strong> 08037040092</p>
+    <section className="civil">
+    <h3>CIVILIAN PLATFORM</h3>
+    <p>Our Institute also runs Community Policing Platform.For Civilians where immense benefits can be enjoyed as follows:</p>
+    <ul>
+        <li>Certificate Course on Security.</li>
+        <li>Free Legal Advice.</li>
+        <li>Conflicts/Alternative Dispute Resolution.</li>
+        <li>Complaint/Petition Writtings.</li>
+        <li>Intervention in Police/Law Enforcement Cases.</li>
+        <li>Bail Bond Processses and Guarantor.</li>
+        <li>Private Investigation Dutiess.</li>
+        <li>Defend of Human Righs.</li>
+        <li>Counseling on Rtirement plans.</li>
+        <li>Road Accident Investigations and Insurance .</li>
+        <li>Free Interet Loan.</li>
+        <li>Dept bying and Finance.</li>
+        <li>Medical Monetary Support.</li>
+        <li>#2M Pension Plan.</li>
+    </ul>
+    <h5 className="protect">PROTECT YOUR JOB AND NEVER WAIT UNTIL TOO LATE !</h5>
+    <p className="phone"><strong>CALL</strong> 08037040092</p>
+    </section>
+    <footer>This website app is created by <strong>opeoluwa </strong>.whatsapp or call<code> 08160698617.</code></footer>
+    
+    
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
